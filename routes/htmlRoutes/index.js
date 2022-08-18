15516eb31ac328ab0bf9d658d7d89b0fec3d2b2b
Andrew Seymour <andrew.seymour1@gmail.com>
2022-08-17 20:43:22 -0500

    added functionality for zookeepers

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,25 +1,20 @@
 const path = require("path");
 const router = require("express").Router();
-const PORT = process.env.PORT || 3006;
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./index.html"));
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 router.get("/animals", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/animals.html"));
+  res.sendFile(path.join(__dirname, "../../public/animals.html"));
 });
 
 router.get("/zookeepers", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+  res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
 });
 
 router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-router.get(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,41 @@
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
-const fs = require("fs");
-const path = require("path");
+// const apiRoutes = require("./routes/apiRoutes");
+// const htmlRoutes = require("./routes/htmlRoutes");
+// const fs = require("fs");
+// const path = require("path");
+// const express = require("express");
+// const { animals } = require("./data/animals");
+// const PORT = process.env.PORT || 3006;
+// const app = express();
+
+// // parse incoming string or array data
+// app.use(express.urlencoded({ extended: true }));
+// // parse incoming JSON data
+// app.use(express.json());
+// app.use(express.static("public"));
+
+// // use apiRoutes
+// app.use("/api", apiRoutes);
+// app.use("/", htmlRoutes);
+
+// app.listen(PORT, () => {
+//   console.log(`API server now on port ${PORT}!`);
+// });
+
 const express = require("express");
-const { animals } = require("./data/animals");
+
 const PORT = process.env.PORT || 3006;
 const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
+app.use(express.static("public"));
+
+// Use apiRoutes
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.use(express.static("public"));
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
